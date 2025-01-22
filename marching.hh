371dfa8c4f5c4ba30bd2a18a158ffcdce0cc1b6e
Peter Salvi <vukung@yahoo.com>
2025-01-22 18:17:46 +0100

    Add namespace

--- marching.hh
+++ marching.hh
@@ -6,7 +6,11 @@
 // Constructs a mesh approximating the zero isosurface of a given scalar function.
 // Evaluation area is a cube centered at `center` with edge length `2*size`.
 
+namespace MarchingCubes {
+
 Geometry::TriMesh isosurface(std::function<double(const Geometry::Point3D &)> f,
                              const Geometry::Point3D &center, double size,
                              size_t min_depth, size_t max_depth);
+
+}
 
